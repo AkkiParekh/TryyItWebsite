0c1e0ec2bad1b14217276398c061af2ebf3e1e6f
Ankit Parekh <AParekh@codifyd.com>
2018-03-26 19:30:06 +0530

    commitinng changes for features page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,9 @@
 (function($) {
     'use strict';
 	$(document).ready(function(){
-		
+		$(".hamburger").click(function() {
+            $("body").hasClass("open") ? $("body").removeClass("open") : $("body").addClass("open")
+        });
 		$(".trBtns").on("click", function(){
 			var ind = $(this).attr("data-trial");
 			var folderName = 'assets/img/homeTrialDemo/' + ind + '/1.png';
@@ -35,5 +37,19 @@
             	$("#memodel-render").attr('src', folderName).attr("data-slide", curr);
         	}).fadeIn(100);
 		});
+
+		$('.randomHover').on('mouseover',function() {
+		    var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+		    $('.randomHover:after').css('background', color);		    
+		});
+
+		if($('#myModal').length){
+			$('#myModal').on('shown.bs.modal', function () {
+	            $('#myModal .modal-body iframe').attr("src", "https://player.vimeo.com/video/244612496?autoplay=1&loop=1");
+	        });
+	        $('#myModal').on('hidden.bs.modal', function () {
+	            $('#myModal .modal-body iframe').attr("src", "");
+	        });
+		}
 	});
 })(jQuery);
